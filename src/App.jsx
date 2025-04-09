c78f092a03f281ebd45cae46ea81335b8986d8bc
Your Name <you@example.com>
2025-04-10 01:14:03 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-green-500">
+                Netflix GPT
+            </h1>
+        </div>
+    )
+}
+
+export default App
